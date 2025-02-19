3200e8435f10f7a2d65e21c7c7a0e1e41b1ee773
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2025-02-19 04:22:19 +0100

    finished django setup

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    buy_link = models.CharField(max_length=2083)
+    image_url = models.CharField(max_length=2083)
+    ranking = models.IntegerField()
+
+
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
+from .models import Product
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the products index.")
+    products = Product.objects.all().order_by('ranking')
+    return render(request, 'index.html', {'products': products})
+    #return HttpResponse("Hello, world. You're at the products index.")
+
+def details(request):
+    return HttpResponse("You're looking at details")
     
